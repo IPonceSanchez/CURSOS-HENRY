6bf3249b233b24aca7d08769cfa2608d8c0260da
IPonceSanchez <vanp1768@gmail.com>
2024-07-13 15:13:33 -0500

    Ejercicios 2

--- C6 Bucles/ejercicios/01.js
+++ C6 Bucles/ejercicios/01.js
@@ -2,7 +2,7 @@ function esIgualYNegativo(a, b) {
   // La función recibe dos argumentos "a" y "b" que son números.
   // Determina si son iguales y son ambos negativos.
   // De ser así, retorna true, de lo contrario, retorna false.
-  // Tu código:
+  // Tu código: PRUEBA PARA SABER SI GUARDA EN EL GIT HUB 
   if(a==b && a<0){
     return true;
   }else return false;
